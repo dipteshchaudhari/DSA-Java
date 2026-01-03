bde153fca09d5175edf9d6c49385a9d397c669a8
dipteshchaudhari <dipteshchaudhary303@gmail.com>
2026-01-04 01:42:50 +0530

    Sliding Window patterns

---
+++ Data Structure/pattern_questions/Sliding Window/First_Question.java
@@ -0,0 +1,61 @@
+// link : https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
+
+
+public class First_Question{
+    public static void main(String[] args) {
+        int arr[] = {100, 200, 300, 400};
+        int k = 2;
+        System.out.println(subArr2(arr, k));
+    }
+    public static int subArr(int arr[],int k){
+        // Using For Loop
+
+        int start = 0;
+        int end = k-1;
+        
+        int max = Integer.MIN_VALUE;
+
+        while (end < arr.length) {
+            int sum = 0;    
+            for(int i=start; i<=end; i++) {
+                sum = sum + arr[i];
+            }
+            max = sum;
+            
+            // sliding window
+            for(end=k; end <arr.length; end++){
+                sum = sum + arr[end] - arr[start];
+                start++;
+                max = Math.max(sum,max);
+            }
+        }  
+        return max; 
+    }
+    public static int subArr2(int arr[],int k){
+        // Using while loop
+
+        int start = 0;
+        int end = k-1;
+        int sum = 0;
+
+        for(int i=start; i<=end; i++) {
+            sum += arr[i];
+        }
+        int max = sum;
+        
+        while (end  < arr.length) {
+
+           max = Math.max(sum, max);
+           start++;
+           end++;
+
+           sum = sum - arr[start];
+           if(end == arr.length){
+            break;
+           }
+           sum = sum + arr[end];
+           
+        }
+        return max;
+    }
+}
